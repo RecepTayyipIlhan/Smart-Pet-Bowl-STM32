4e4b5cc878c10e4855b18959f4ff2f6315712a2e
unknown <ilhanreceptayyip@outlook.com>
2022-05-31 18:29:52 +0300

    first commit

---
+++ MDK-ARM/SmartPetBowl/main.d
@@ -0,0 +1,38 @@
+smartpetbowl\main.o: ../Core/Src/main.c
+smartpetbowl\main.o: ../Core/Inc/main.h
+smartpetbowl\main.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h
+smartpetbowl\main.o: ../Core/Inc/stm32f0xx_hal_conf.h
+smartpetbowl\main.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h
+smartpetbowl\main.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_def.h
+smartpetbowl\main.o: ../Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h
+smartpetbowl\main.o: ../Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f051x8.h
+smartpetbowl\main.o: ../Drivers/CMSIS/Include/core_cm0.h
+smartpetbowl\main.o: D:\Software Programs\Keil\ARM\ARMCC\Bin\..\include\stdint.h
+smartpetbowl\main.o: ../Drivers/CMSIS/Include/cmsis_version.h
+smartpetbowl\main.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
+smartpetbowl\main.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
+smartpetbowl\main.o: ../Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h
+smartpetbowl\main.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h
+smartpetbowl\main.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h
+smartpetbowl\main.o: D:\Software Programs\Keil\ARM\ARMCC\Bin\..\include\stddef.h
+smartpetbowl\main.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h
+smartpetbowl\main.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio.h
+smartpetbowl\main.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h
+smartpetbowl\main.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_exti.h
+smartpetbowl\main.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h
+smartpetbowl\main.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h
+smartpetbowl\main.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h
+smartpetbowl\main.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_adc.h
+smartpetbowl\main.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_adc_ex.h
+smartpetbowl\main.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h
+smartpetbowl\main.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h
+smartpetbowl\main.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h
+smartpetbowl\main.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h
+smartpetbowl\main.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr.h
+smartpetbowl\main.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h
+smartpetbowl\main.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_tim.h
+smartpetbowl\main.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_tim_ex.h
+smartpetbowl\main.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart.h
+smartpetbowl\main.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart_ex.h
+smartpetbowl\main.o: D:\Software Programs\Keil\ARM\ARMCC\Bin\..\include\string.h
+smartpetbowl\main.o: D:\Software Programs\Keil\ARM\ARMCC\Bin\..\include\stdio.h
---
+++ MDK-ARM/SmartPetBowl/stm32f0xx_hal.d
@@ -0,0 +1,35 @@
+smartpetbowl\stm32f0xx_hal.o: ../Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal.c
+smartpetbowl\stm32f0xx_hal.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h
+smartpetbowl\stm32f0xx_hal.o: ../Core/Inc/stm32f0xx_hal_conf.h
+smartpetbowl\stm32f0xx_hal.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h
+smartpetbowl\stm32f0xx_hal.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_def.h
+smartpetbowl\stm32f0xx_hal.o: ../Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h
+smartpetbowl\stm32f0xx_hal.o: ../Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f051x8.h
+smartpetbowl\stm32f0xx_hal.o: ../Drivers/CMSIS/Include/core_cm0.h
+smartpetbowl\stm32f0xx_hal.o: D:\Software Programs\Keil\ARM\ARMCC\Bin\..\include\stdint.h
+smartpetbowl\stm32f0xx_hal.o: ../Drivers/CMSIS/Include/cmsis_version.h
+smartpetbowl\stm32f0xx_hal.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
+smartpetbowl\stm32f0xx_hal.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
+smartpetbowl\stm32f0xx_hal.o: ../Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h
+smartpetbowl\stm32f0xx_hal.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h
+smartpetbowl\stm32f0xx_hal.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h
+smartpetbowl\stm32f0xx_hal.o: D:\Software Programs\Keil\ARM\ARMCC\Bin\..\include\stddef.h
+smartpetbowl\stm32f0xx_hal.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h
+smartpetbowl\stm32f0xx_hal.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio.h
+smartpetbowl\stm32f0xx_hal.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h
+smartpetbowl\stm32f0xx_hal.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_exti.h
+smartpetbowl\stm32f0xx_hal.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h
+smartpetbowl\stm32f0xx_hal.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h
+smartpetbowl\stm32f0xx_hal.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h
+smartpetbowl\stm32f0xx_hal.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_adc.h
+smartpetbowl\stm32f0xx_hal.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_adc_ex.h
+smartpetbowl\stm32f0xx_hal.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h
+smartpetbowl\stm32f0xx_hal.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h
+smartpetbowl\stm32f0xx_hal.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h
+smartpetbowl\stm32f0xx_hal.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h
+smartpetbowl\stm32f0xx_hal.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr.h
+smartpetbowl\stm32f0xx_hal.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h
+smartpetbowl\stm32f0xx_hal.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_tim.h
+smartpetbowl\stm32f0xx_hal.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_tim_ex.h
+smartpetbowl\stm32f0xx_hal.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart.h
+smartpetbowl\stm32f0xx_hal.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart_ex.h
